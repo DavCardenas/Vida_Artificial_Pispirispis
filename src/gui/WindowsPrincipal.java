4ad17088a6eb49c09c4ef341601bfd14cbe12b53
Leonardo <Leonardo@Leonardo-PC>
2018-05-16 22:27:10 -0500

    Primer Archivo ahora si

---
+++ src/gui/WindowsPrincipal.java
@@ -0,0 +1,26 @@
+package gui;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class WindowsPrincipal extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6169301488090101321L;
+	
+	public static final int WIDTH = 1000;
+	public static final int HEIGHT = 600;
+	public static final String TITLE = "Vida Artificial - Pispirispis";
+	
+	public WindowsPrincipal() {
+		setSize(new Dimension(WIDTH, HEIGHT));
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setResizable(false);
+		setTitle(TITLE);
+	}
+
+}
---
+++ src/run/Run.java
@@ -0,0 +1,12 @@
+package run;
+
+import gui.WindowsPrincipal;
+
+public class Run {
+
+	public static void main(String[] args) {
+		WindowsPrincipal windowsP = new WindowsPrincipal();
+		windowsP.setVisible(true);
+	}
+
+}
