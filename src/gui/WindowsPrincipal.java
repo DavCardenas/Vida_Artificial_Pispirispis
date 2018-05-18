d596316afe17973783266fc88e8c7d3613aed87f
Sebastian <Sebastian@DESKTOP-DUA3SHR>
2018-05-18 00:34:49 -0500

    Se hace para dar los valore iniciales por interfaz, algunas veces pinta bien la grafica, otras no

--- src/gui/WindowsPrincipal.java
+++ src/gui/WindowsPrincipal.java
@@ -1,8 +1,19 @@
 package gui;
 
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
@@ -21,13 +32,37 @@ public class WindowsPrincipal extends JFrame implements Runnable{
 	private static final long serialVersionUID = -6169301488090101321L;
 	private ChartPanel chartPanel;
 	
-	public static final int WIDTH = 1000;
-	public static final int HEIGHT = 600;
+	public static final int WIDTH = 600;
+	public static final int HEIGHT = 400;
 	public static final String TITLE = "Vida Artificial - Pispirispis";
 	
 	
 	private Simulation simulation;
 	private Thread thread;
+	
+	private JButton btnInitSimulation;
+	
+	private JLabel lbSize, lbPoblation,lbFood, lbEnergy,
+			lbAlicanola,lbRateFood,lbMale,lbFemale,lbInopios,lbTropus, lbTime,lbFifirufas;
+	private JTextField txtSize, txtPoblation,txtFood, txtEnergy,
+		txtAlicanola,txtRateFood,txtMale,txtFemale,txtInopios,txtTropus, txtTime,txtFifirufas;
+	
+	private JDialog dialogGraphic;
+	
+	public double size; // superficie en metros cuadrados
+	private int init_poblation; // poblacion inicial
+	private int init_food; // cantidad comida inicial
+	private double init_energy; // cantidad de energia total para los pispirispis
+	private double init_energy_alicanola; // cantidad de energia que aportan las alicanolas
+	public double init_energy_fifirufa; // cantidad de energia que quitan las fifirufas
+	private double rate_food; // tasa de generacion de alimentos
+	private double rate_male; // porcentaje de machos
+	private double rate_female; // porcentaje de hembras
+	private double rate_inpopios; // porcentaje de inopios
+	private double rate_tropus; // porcentaje de tropus
+	public  int time_simulation; // tiempo que dura la simulacion en años
+	
+	private GridBagConstraints constraints;
 	
 	public WindowsPrincipal() {
 		setSize(new Dimension(WIDTH, HEIGHT));
@@ -35,28 +70,156 @@ public class WindowsPrincipal extends JFrame implements Runnable{
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setResizable(false);
 		setTitle(TITLE);
-		this.initSimulation();
-		JFreeChart freeChart = ChartFactory.createLineChart("Pispirispis","Dìas","Hombres",getDefaultCategoryDataset(),
-				PlotOrientation.VERTICAL,true,true,false);
-		this.chartPanel = new ChartPanel(freeChart);
-		chartPanel.setPreferredSize(new Dimension(500, 500));
-		setContentPane(chartPanel);
-		chartPanel.setVisible(true);
-		this.thread = new Thread(this);
-		this.thread.start();
+		this.setLayout(new GridBagLayout());
+		this.constraints = new GridBagConstraints();
+		thread = new Thread(this);
+		this.dialogGraphic = new JDialog(this,true);
+		this.dialogGraphic.setSize(new Dimension(500, 500));
+		this.dialogGraphic.setLocationRelativeTo(null);
+		this.initComponents();
+		this.addComponents();
+		this.activateButton();
+//		this.initSimulation();
+		
 				
 	}
 	
+	public void initComponents() {
+		this.initLabels();
+		this.initTextFields();
+		this.initButtons();
+	}
+	
+	public void initLabels() {
+		this.lbSize = new JLabel("Tamaño (m^2): ");
+		this.lbPoblation = new JLabel("Población (u): ");
+		this.lbFood = new JLabel("Comida (u): ");
+		this.lbEnergy = new JLabel("Energía (u): ");
+		this.lbAlicanola = new JLabel("Alicanola (u): ");
+		this.lbRateFood = new JLabel("Tasa generacion de alimentos (%): ");
+		this.lbMale = new JLabel("Machos (%): ");
+		this.lbFemale = new JLabel("Hembras (%): ");
+		this.lbInopios = new JLabel("Inopios (%): ");
+		this.lbTropus = new JLabel("Tropus (%): ");
+		this.lbTime = new JLabel("Tiempo (años): ");
+		this.lbFifirufas = new JLabel("Desgaste por fifirufs (energía): ");
+	}
+	
+	public void initTextFields() {
+		this.txtSize = new JTextField(15);
+		this.txtPoblation = new JTextField(15);
+		this.txtFood = new JTextField(15);
+		this.txtEnergy = new JTextField(15);
+		this.txtAlicanola = new JTextField(15);
+		this.txtRateFood = new JTextField(15);
+		this.txtMale = new JTextField(15);
+		this.txtFemale = new JTextField(15);
+		this.txtInopios = new JTextField(15);
+		this.txtTropus = new JTextField(15);
+		this.txtTime = new JTextField(15);
+		this.txtFifirufas = new JTextField(15);
+	}
+	
+	public void initButtons() {
+		this.btnInitSimulation = new JButton("Iniciar Simulación");
+	}
+	
+	public void addComponent(Component component,int column, int row) {
+		this.constraints.weightx = 0.5;
+		this.constraints.gridx = column;
+		this.constraints.gridy = row;
+		this.constraints.weighty = 1.0;
+		this.add(component,constraints);
+	}
+	
+	public void addComponents() {
+		this.addComponent(this.lbSize, 0, 0);
+		this.addComponent(this.txtSize, 1, 0);
+		this.addComponent(this.lbPoblation, 0, 1);
+		this.addComponent(this.txtPoblation, 1, 1);
+	
+		this.addComponent(this.lbFood, 0, 2);
+		this.addComponent(this.txtFood, 1, 2);
+	
+		this.addComponent(this.lbEnergy, 0, 3);
+		this.addComponent(this.txtEnergy, 1, 3);
+	
+		this.addComponent(this.lbAlicanola, 0, 4);
+		this.addComponent(this.txtAlicanola, 1, 4);
+	
+		this.addComponent(this.lbRateFood, 0, 5);
+		this.addComponent(this.txtRateFood, 1, 5);
+	
+		this.addComponent(this.lbMale, 0, 6);
+		this.addComponent(this.txtMale, 1, 6);
+	
+		this.addComponent(this.lbFemale, 0, 7);
+		this.addComponent(this.txtFemale, 1, 7);
+	
+		this.addComponent(this.lbInopios, 0, 8);
+		this.addComponent(this.txtInopios, 1, 8);
+	
+		this.addComponent(this.lbTropus, 0, 9);
+		this.addComponent(this.txtTropus, 1, 9);
+	
+		this.addComponent(this.lbTime, 0, 10);
+		this.addComponent(this.txtTime, 1, 10);
+	
+		this.addComponent(this.lbFifirufas, 0, 11);
+		this.addComponent(this.txtFifirufas, 1, 11);
+		this.constraints.gridwidth = 2;
+		this.addComponent(this.btnInitSimulation, 0, 12);
+	
+	}
+	
+	public void getValuesFromComponents() {
+		try {
+			size = Double.parseDouble(this.txtSize.getText());
+			init_poblation=Integer.parseInt(this.txtPoblation.getText()); 
+			init_food=Integer.parseInt(this.txtFood.getText());
+			init_energy=Double.parseDouble(this.txtEnergy.getText());
+			init_energy_alicanola=Double.parseDouble(this.txtAlicanola.getText());
+			init_energy_fifirufa=Double.parseDouble(this.txtFifirufas.getText()); 
+			rate_food=Double.parseDouble(this.txtRateFood.getText()); 
+			rate_male=Double.parseDouble(this.txtMale.getText()); 
+			rate_female=Double.parseDouble(this.txtFemale.getText());
+			rate_inpopios=Double.parseDouble(this.txtInopios.getText());
+			rate_tropus=Double.parseDouble(this.txtTropus.getText());
+			time_simulation=Integer.parseInt(this.txtTime.getText());
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Error en los datos");
+		}
+	}
+	
 	public void initSimulation() {
-		this.simulation = new Simulation(100, 10,10,6,2,0.3,0.6,0.4,0.2,0.8,2,3);
+		this.simulation = new Simulation(size, init_poblation,init_food,init_energy,
+				init_energy_alicanola,rate_food/100,rate_male/100,rate_female/100,rate_inpopios/100,
+				rate_tropus/100,time_simulation,init_energy_fifirufa);
 		this.simulation.startSimulation();		
 	}
 	
+	public void activateButton() {
+		this.btnInitSimulation.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				getValuesFromComponents();
+				initSimulation();
+				JFreeChart freeChart = ChartFactory.createLineChart("Pispirispis","Dìas","Hombres",getDefaultCategoryDataset(),
+						PlotOrientation.VERTICAL,true,true,false);
+				chartPanel = new ChartPanel(freeChart);
+				chartPanel.setPreferredSize(new Dimension(500, 500));
+				dialogGraphic.add(chartPanel);
+				chartPanel.setVisible(true);
+				dialogGraphic.setVisible(true);
+				thread.start();
+			}
+		});
+	}
 	
 	public DefaultCategoryDataset getDefaultCategoryDataset() {
 		DefaultCategoryDataset categoryDataset = new DefaultCategoryDataset();
-		for (int i = 0; i < this.simulation.
-				getListMale().size(); i++) {
+		for (int i = 0; i < this.simulation.getListMale().size(); i++) {
 			categoryDataset.addValue(this.simulation.getListMale().get(i).
 					getValue(), "Hombre", ""+this.simulation.getListMale().
 					get(i).getDay());
@@ -74,6 +237,7 @@ public class WindowsPrincipal extends JFrame implements Runnable{
 //			chartPanel = new ChartPanel(freeChart);
 						chartPanel.setChart(freeChart);
 						chartPanel.repaint();
+						dialogGraphic.repaint();
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
